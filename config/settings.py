9d34fe9090771a9d284e89efc4f9daf18b6aa2ee
Roein-yousefi <roeinyousefi@gmail.com>
2024-08-04 17:51:45 +0330

    create app products and model

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
     'accounts',
     'pages',
+    'products',
 ]
 
 SITE_ID = 1
---
+++ products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+
+    datetime_added = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
